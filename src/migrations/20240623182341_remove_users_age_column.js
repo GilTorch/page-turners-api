b540eb0200d88939c8911d2c72bd5e71447ee5d2
GilTorch <infotorch2014@gmail.com>
2024-06-23 14:34:49 -0400

    Remove age column

---
+++ src/migrations/20240623182341_remove_users_age_column.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  
+    return knex.schema.alterTable('users', function (table) {
+        table.dropColumn('age');
+    });
+
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+    return knex.schema.alterTable('users', function (table) {
+        table.integer('age');
+    });
+
+};
